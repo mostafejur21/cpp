f3d37a5fda750168a5da40e31dec33707683e351
Mostafejur Rahman <106027543+mostafejur21@users.noreply.github.com>
2022-09-10 01:15:11 +0600

    Add files via upload

---
+++ printprime.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);        
+        freopen("output.txt","w",stdout);
+    #endif
+
+    int a,b;
+    cin>>a>>b;
+    int num;
+    for(num=a; num<=b;num++){
+    int i;
+        for(i=2;i<num;i++){
+            if(num%i==0){
+                break;
+            }
+        }
+        if(i==num){
+            cout<<num<<endl;
+        }
+    }   
+    return 0;
+}
